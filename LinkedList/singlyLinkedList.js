3b14be72a0418832b8df6b6209d5a038623d6a61
Daniel Emehinola <dan.emehin@gmail.com>
2022-09-11 13:55:57 -0600

    singlyLinkedList implementation

---
+++ LinkedList/singlyLinkedList.js
@@ -0,0 +1,95 @@
+class Node {
+  constructor(value) {
+    (this.value = value), (this.next = null);
+  }
+}
+
+class SinglyLinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+  }
+
+  insert(index, value) {
+    if (index === 0) {
+      this.prepend(value);
+      return this.printList();
+    } else if (index >= this.length) {
+      this.append(value);
+      return this.printList();
+    }
+
+    const newNode = new Node(value);
+    const previous = this.listTraversal(index - 1);
+    const next = this.listTraversal(index);
+    newNode.next = next;
+    previous.next = newNode;
+    this.length++;
+  }
+
+  remove(index) {
+    if (index === 0) {
+      this.head = this.head.next;
+      return this.printList();
+    } else if (index === this.length - 1) {
+      this.tail = this.listTraversal(index - 1);
+      this.tail.next = null;
+      return this.printList();
+    }
+    const item = this.listTraversal(index);
+    const previous = this.listTraversal(index - 1);
+    const next = this.listTraversal(index + 1);
+    previous.next = next;
+    this.length--;
+    return item;
+  }
+
+  listTraversal(index) {
+    let count = 0;
+    let currentNode = this.head;
+    while (count !== index) {
+      currentNode = currentNode.next;
+      count++;
+    }
+    return currentNode;
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+}
+
+myLinkedList = new SinglyLinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+myLinkedList.insert(2, 99);
+myLinkedList.insert(0, 9);
+myLinkedList.insert(20, 100);
+myLinkedList.remove(2);
+// console.log(myLinkedList);
+console.log(myLinkedList.printList());
