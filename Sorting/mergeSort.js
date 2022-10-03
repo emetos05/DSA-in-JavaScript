2d1ea27a1a08a2916c9781c0f17821941182dc36
Daniel Emehinola <dan.emehin@gmail.com>
2022-10-03 08:42:11 -0600

    Insertion and merge sort Implementation

---
+++ Sorting/mergeSort.js
@@ -0,0 +1,37 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function mergeSort(array) {
+  if (array.length === 1) {
+    return array;
+  }
+
+  // Split array into two half
+  const length = array.length;
+  const middle = Math.floor(length / 2);
+  const left = array.slice(0, middle);
+  const right = array.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  const result = [];
+  length = left.length + right.length;
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  // Compare items in split arrays
+  while (result.length < length) {
+    if (right.length === rightIndex || left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return result;
+}
+
+const answer = mergeSort(numbers);
+console.log(answer);
