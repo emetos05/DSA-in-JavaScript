63836099ad9e189a997a6dffd1d851b1b48b1174
Daniel Emehinola <dan.emehin@gmail.com>
2023-01-14 14:54:18 -0600

    Unit test implemented

--- S_Expression_Calculator/calc.spec.js
+++ S_Expression_Calculator/calc.spec.js
@@ -1,138 +1,55 @@
 const assert = require("assert");
-const calc = require("./calc");
+const S_Expression = require("./calc");
 
-describe("SExpression", () => {
-  describe("#calc()", () => {
-    it("should return the input number when no function is called", () => {
-      const sExpressionCalc = new calc.SExpression();
+describe("S_Expression", () => {
+  describe("#calculate()", () => {
+    it("test for single digit value", () => {
+      const input = "5";
+      const expected = 5;
 
-      assert.strictEqual(sExpressionCalc.calc("0"), 0);
-      assert.strictEqual(sExpressionCalc.calc("1"), 1);
-      assert.strictEqual(sExpressionCalc.calc("2"), 2);
-      assert.strictEqual(sExpressionCalc.calc("3"), 3);
-      assert.strictEqual(sExpressionCalc.calc("4"), 4);
-      assert.strictEqual(sExpressionCalc.calc("5"), 5);
-      assert.strictEqual(sExpressionCalc.calc("8"), 8);
-      assert.strictEqual(sExpressionCalc.calc("16"), 16);
-      assert.strictEqual(sExpressionCalc.calc("32"), 32);
-      assert.strictEqual(sExpressionCalc.calc("64"), 64);
-      assert.strictEqual(sExpressionCalc.calc("128"), 128);
-      assert.strictEqual(sExpressionCalc.calc("256"), 256);
-
-      for (let i = 0; i < 1000; i++) {
-        assert.strictEqual(sExpressionCalc.calc(i.toString()), i);
-      }
-
-      for (let i = 0; i < 100; i++) {
-        const val = Math.floor(Math.random() * 10001);
-        assert.strictEqual(sExpressionCalc.calc(val.toString()), val);
-      }
+      const result = new S_Expression().calculate(input);
+      assert.strictEqual(result, expected);
     });
 
-    it("should perform addition correctly", () => {
-      const sExpressionCalc = new calc.SExpression();
+    it("test for multi digit value", () => {
+      const input = "23";
+      const expected = 23;
 
-      assert.strictEqual(sExpressionCalc.calc("(add 1 1)"), 2);
-      assert.strictEqual(sExpressionCalc.calc("(add 12 34)"), 46);
-      assert.strictEqual(sExpressionCalc.calc("(add 321 456)"), 777);
-
-      for (let i = 0; i < 100; i++) {
-        for (let j = 0; j < 100; j++) {
-          assert.strictEqual(sExpressionCalc.calc(`(add ${i} ${j})`), i + j);
-        }
-      }
-
-      for (let i = 0; i < 100; i++) {
-        const a = Math.floor(Math.random() * 10001);
-        const b = Math.floor(Math.random() * 10001);
-        assert.strictEqual(sExpressionCalc.calc(`(add ${a} ${b})`), a + b);
-      }
+      const result = new S_Expression().calculate(input);
+      assert.strictEqual(result, expected);
     });
 
-    it("should perform multiplication correctly", () => {
-      const sExpressionCalc = new calc.SExpression();
+    it("test for addition", () => {
+      const input = "(add 2 3)";
+      const expected = 5;
 
-      assert.strictEqual(sExpressionCalc.calc("(multiply 1 1)"), 1);
-      assert.strictEqual(sExpressionCalc.calc("(multiply 12 4)"), 48);
-      assert.strictEqual(sExpressionCalc.calc("(multiply 321 456)"), 146376);
-      for (let i = 0; i < 100; i++) {
-        for (let j = 0; j < 100; j++) {
-          assert.strictEqual(
-            sExpressionCalc.calc(`(multiply ${i} ${j})`),
-            i * j
-          );
-        }
-      }
-
-      for (let i = 0; i < 100; i++) {
-        const a = Math.floor(Math.random() * 10001);
-        const b = Math.floor(Math.random() * 10001);
-        assert.strictEqual(sExpressionCalc.calc(`(multiply ${a} ${b})`), a * b);
-      }
+      const result = new S_Expression().calculate(input);
+      assert.strictEqual(result, expected);
     });
 
-    it("should handle nested function calls correctly", () => {
-      const sExpressionCalc = new calc.SExpression();
+    it("test for multiplication", () => {
+      const input = "(multiply 2 10)";
+      const expected = 20;
 
-      assert.strictEqual(sExpressionCalc.calc("(add (add 1 2) 5)"), 8);
-      assert.strictEqual(sExpressionCalc.calc("(add (multiply 2 3) 7)"), 13);
+      const result = new S_Expression().calculate(input);
+      assert.strictEqual(result, expected);
+    });
 
-      assert.strictEqual(sExpressionCalc.calc("(add 3 (add 1 2))"), 6);
-      assert.strictEqual(sExpressionCalc.calc("(add 3 (multiply 1 2))"), 5);
+    it("test for nested expression", () => {
+      const input = "(multiply 2 (add (multiply 2 3) 8))";
+      const expected = 28;
 
-      assert.strictEqual(sExpressionCalc.calc("(add (add 1 2) (add 3 4))"), 10);
-      assert.strictEqual(
-        sExpressionCalc.calc("(add (add 1 2) (multiply 1 2))"),
-        5
-      );
-      assert.strictEqual(
-        sExpressionCalc.calc("(add (multiply 5 3) (add 3 4))"),
-        22
-      );
-      assert.strictEqual(
-        sExpressionCalc.calc("(add (multiply 5 3) (multiply 3 4))"),
-        27
-      );
+      const result = new S_Expression().calculate(input);
+      assert.strictEqual(result, expected);
+    });
 
-      assert.strictEqual(sExpressionCalc.calc("(multiply (add 1 2) 5)"), 15);
-      assert.strictEqual(
-        sExpressionCalc.calc("(multiply (multiply 2 3) 7)"),
-        42
-      );
+    it("should throw an error for an invalid operator", () => {
+      const input = "(invalid_operator 1 2 3)";
 
-      assert.strictEqual(sExpressionCalc.calc("(multiply 3 (add 1 2))"), 9);
-      assert.strictEqual(
-        sExpressionCalc.calc("(multiply 3 (multiply 1 2))"),
-        6
-      );
-
-      assert.strictEqual(
-        sExpressionCalc.calc("(multiply (add 1 2) (add 3 4))"),
-        21
-      );
-      assert.strictEqual(
-        sExpressionCalc.calc("(multiply (add 1 2) (multiply 1 2))"),
-        6
-      );
-      assert.strictEqual(
-        sExpressionCalc.calc("(multiply (multiply 5 3) (add 3 4))"),
-        105
-      );
-      assert.strictEqual(
-        sExpressionCalc.calc("(multiply (multiply 5 3) (multiply 3 4))"),
-        180
-      );
-
-      // More complex combinations
-      assert.strictEqual(
-        sExpressionCalc.calc("(multiply (add (multiply 1 2) 3) (add 1 2))"),
-        15
-      );
-      assert.strictEqual(
-        sExpressionCalc.calc(
-          "(multiply (add (multiply 1 2) (add 3 4)) (multiply (add 5 6) (multiply 7 8)))"
-        ),
-        5544
+      assert.throws(
+        () => new S_Expression().calculate(input),
+        Error,
+        'Invalid operator "invalid_operator"'
       );
     });
   });
